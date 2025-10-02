0be20a4074297e4d0ea6643856bc7e01809e0d5d
SaaraShaikh77 <shaikhsaara77@gmail.com>
2025-10-03 01:14:57 +0530

    Resolve merge conflicts in main.jsx and other files

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-  import React from 'react';
-  import ReactDOM from 'react-dom/client';
-  import './index.css'; // This line imports Tailwind
-  import BirthdayWebsite from './App.jsx';
+     import React from 'react';
+     import ReactDOM from 'react-dom/client';
+     import App from './App.jsx';
+     import './index.css';
 
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <BirthdayWebsite />
-    </React.StrictMode>
-  );
-  
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> f7c4ab326e50502d8034fa6fd92f3e24b86d2c57
+     ReactDOM.createRoot(document.getElementById('root')).render(
+       <React.StrictMode>
+         <App />
+       </React.StrictMode>,
+     );
+     
